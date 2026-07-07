52d79a0ea29b65d31a0236cf862924c89e167139
agcblue <alexchirvasie@gmail.com>
2026-07-07 11:22:06 +0300

    Testing

---
+++ src/test/java/agc/heimdall/TestLogIngestion.java
@@ -0,0 +1,29 @@
+package agc.heimdall;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
+import org.springframework.boot.test.context.SpringBootTest;
+import agc.heimdall.LogIngestion.*;
+
+@SpringBootTest
+class TestLogIngestion
+{
+	@Test
+	public void mapTest(TestReporter reporter) 
+    {
+		LogIngestion loggs = new LogIngestion();
+        Map<String, String> map = loggs.getLogConfig();
+
+        assertEquals("abc", map.get("test/test0"));
+        assertEquals("def", map.get("test/test0/test1"));
+	}	
+
+}
+
+
